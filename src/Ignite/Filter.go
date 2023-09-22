17ead3bf37f5890ed423e128882a456375e5a1c1
antegral <antegral@antegral.net>
2023-09-23 04:08:41 +0900

    .

---
+++ src/Ignite/Filter.go
@@ -0,0 +1,13 @@
+package Ignite
+
+import "antegr.al/chatanium-bot/v1/src/Log"
+
+func GetModulesByACL(GuildID string) []string {
+	switch GuildID {
+	case "919823370600742942":
+		return []string{"ping", "echo"}
+	default:
+		Log.Warn.Printf("%s > No ACL defined, using default", GuildID)
+		return []string{}
+	}
+}
