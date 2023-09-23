cf65289b3f4b167eded55cf67796ff34f3916458
antegral <antegral@antegral.net>
2023-09-24 01:39:51 +0900

    .

--- database/Schema.sql
+++ database/Schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "public".attechments
 (
-    "id"         bigint NOT NULL,
+    "id"       bigint NOT NULL,
     message_id bigint NOT NULL,
     content    text NOT NULL,
     CONSTRAINT PK_1 PRIMARY KEY ( "id" ),
@@ -14,12 +14,12 @@ CREATE INDEX FK_1 ON "public".attechments
 
 CREATE TABLE "public".channels
 (
-    "id"          bigint NOT NULL,
-    guild_id    bigint NOT NULL,
-    name        varchar(25) NOT NULL,
-    description varchar(1000) NULL,
-    created_at  timestamp NOT NULL,
-    deleted_at  timestamp NULL,
+    "id"            bigint NOT NULL,
+    guild_id        bigint NOT NULL,
+    name            varchar(25) NOT NULL,
+    description     varchar(1000) NULL,
+    created_at      timestamp NOT NULL,
+    deleted_at      timestamp NULL,
     CONSTRAINT PK_1 PRIMARY KEY ( "id" ),
     CONSTRAINT FK_7 FOREIGN KEY ( guild_id ) REFERENCES "public".guilds ( "id" )
 );
@@ -32,8 +32,8 @@ CREATE INDEX FK_1 ON "public".channels
 CREATE TABLE "public".guilds
 (
     "id"       bigint NOT NULL,
-    name     varchar(100) NOT NULL,
-    owner_id bigint NOT NULL,
+    name       varchar(100) NOT NULL,
+    owner_id   bigint NOT NULL,
     CONSTRAINT PK_1 PRIMARY KEY ( "id" ),
     CONSTRAINT FK_2 FOREIGN KEY ( owner_id ) REFERENCES "public".users ( "id" )
 );
@@ -66,18 +66,17 @@ CREATE INDEX FK_2 ON "public".guildUsers
 
 CREATE TABLE "public".kvStorages
 (
-    uuid    uuid NOT NULL,
-    user_id bigint NOT NULL,
-    key     text NOT NULL,
-    value   text NOT NULL,
-    CONSTRAINT PK_1 PRIMARY KEY ( uuid ),
-    CONSTRAINT FK_6 FOREIGN KEY ( user_id ) REFERENCES "public".users ( "id" )
+    user_id     bigint NOT NULL,
+    key         text NOT NULL,
+    value       text NOT NULL,
+    CONSTRAINT  PK_1 PRIMARY KEY ( user_id ),
+    CONSTRAINT  FK_6 FOREIGN KEY ( user_id ) REFERENCES "public".users ( "id" )
 );
 
 CREATE INDEX FK_1 ON "public".kvStorages
 (
     user_id
-);
+)
 
 CREATE TABLE "public".messages
 (
@@ -89,10 +88,10 @@ CREATE TABLE "public".messages
     contents     text NULL,
     reference_id bigint NULL,
     created_at   timestamp NOT NULL,
-    CONSTRAINT PK_1 PRIMARY KEY ( message_id ),
-    CONSTRAINT FK_4 FOREIGN KEY ( guild_id ) REFERENCES "public".guilds ( "id" ),
-    CONSTRAINT FK_5 FOREIGN KEY ( user_id ) REFERENCES "public".users ( "id" ),
-    CONSTRAINT FK_10 FOREIGN KEY ( channel_id ) REFERENCES "public".channels ( "id" )
+    CONSTRAINT   PK_1 PRIMARY KEY ( message_id ),
+    CONSTRAINT   FK_4 FOREIGN KEY ( guild_id ) REFERENCES "public".guilds ( "id" ),
+    CONSTRAINT   FK_5 FOREIGN KEY ( user_id ) REFERENCES "public".users ( "id" ),
+    CONSTRAINT   FK_10 FOREIGN KEY ( channel_id ) REFERENCES "public".channels ( "id" )
 );
 
 CREATE INDEX FK_1 ON "public".messages
@@ -112,9 +111,22 @@ CREATE INDEX FK_3 ON "public".messages
 
 CREATE TABLE "public".users
 (
-    "id"         bigint NOT NULL,
+    "id"       bigint NOT NULL,
     username   char(32) NOT NULL,
     created_at timestamp NOT NULL,
     deleted_at timestamp NULL,
     CONSTRAINT PK_1 PRIMARY KEY ( "id" )
+);
+
+CREATE TABLE "public".moduleACL
+(
+    "id"             bigint NOT NULL,
+    allowed_modules  text[] NOT NULL,
+    CONSTRAINT       PK_1 PRIMARY KEY ( "id" ),
+    CONSTRAINT       FK_10_1 FOREIGN KEY ( "id" ) REFERENCES "public".channels ( "id" )
+);
+
+CREATE INDEX FK_1 ON "public".moduleACL
+(
+    "id"
 );
